d5c9606fb5b316498730b42fa92bcb2ea7f05dce
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-12-05 16:23:40 +0100

    add hatch 5

---
+++ 5/main.py
@@ -0,0 +1,37 @@
+import itertools
+
+def remove_n(option):
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1]
+
+    out_numbers = []
+    out_options = []
+    for o, curr in zip(option, numbers):
+        if o == "n":
+            old = out_numbers[-1]
+            out_numbers[-1] = old * 10 + curr
+        else:
+            out_numbers.append(curr)
+            out_options.append(o)
+    return zip(out_options, out_numbers)
+
+
+def convert(option):
+    s = 0
+    option = list(option)
+    option.insert(0, "a")
+    for o, n in remove_n(option):
+        if o == "a":
+            s += n
+        elif o == "s":
+            s -= n
+    return s
+
+
+def main():
+    count = 0
+    for c in itertools.product("asn", repeat=14):
+        if convert(c) == 42:
+            count += 1
+            print(c, count)
+
+main()
