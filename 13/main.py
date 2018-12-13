0b9c9ab972f7229a9915bc36c34d06d1781c3c8b
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-12-13 21:41:00 +0100

    Trimming some fat

--- 13/main.py
+++ 13/main.py
@@ -5,15 +5,18 @@ num = [0]*count*2
 num[1] = 1
 num[3] = 1
 
-stack = []
+primesum = 0
+primecount = 0
 
 for i in range(1, count):
     if num[i] == 1:
         if isprime(i):
-            stack.append(i)
-        print(i)
+            primesum += i
+            primecount += 1
+            if primecount == 100:
+                break
         for j in range(1, i):
             if num[j] == 1 and i != j:
                 num[i+j] += 1
 
-print(stack, sum(stack), sum(stack[:100]), len(stack))
+print(primesum, primecount)
