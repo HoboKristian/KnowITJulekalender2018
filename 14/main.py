8184fbe845bd785999465447976393dfa0af0d58
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-12-14 17:16:37 +0100

    Removed some tmp variables

--- 14/main.py
+++ 14/main.py
@@ -1,22 +1,20 @@
-curr = [0,0]
+x,y = 0,0
 actions = {"H": [1,0],
         "V": [-1,0],
         "F": [0,1],
         "B": [0,-1]}
 
 locs = set()
-locs.add((curr[0], curr[1]))
+locs.add((x, y))
 
-moves = 0
 txt = open("./input-bounding-crisscross.txt").read().strip()
 for i in range(0, len(txt), 2):
     c = int(txt[i])
-    moves += c
     a = txt[i+1]
     for j in range(c):
-        curr[0] += actions[a][0]
-        curr[1] += actions[a][1]
-        locs.add((curr[0], curr[1]))
+        x += actions[a][0]
+        y += actions[a][1]
+        locs.add((x, y))
 
 max_x, min_x, max_y, min_y = 0,0,0,0
 for l in locs:
@@ -29,4 +27,4 @@ print(max_x, min_x, max_y, min_y)
 bounding_box = (max_x-min_x+1)*(max_y-min_y+1)
 visited = len(locs)
 not_visited = bounding_box - visited
-print(f"{moves} {bounding_box} {visited} {not_visited} {visited/not_visited:.16f}")
+print(f"{bounding_box} {visited} {not_visited} {visited/not_visited:.16f}")
