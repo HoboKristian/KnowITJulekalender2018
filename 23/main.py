84b172d55aad65a5e47d7ac11a52ac9b7546f73c
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-12-23 13:32:15 +0100

    Change if to filter

--- 23/main.py
+++ 23/main.py
@@ -10,16 +10,18 @@ def main():
     range2 = [5,4,3,2,7,6,5,4,3,2]
 
     count = 0
-    for num in numbers:
-        if int(num[:2]) <= 31 and num[2:4] == "08" and int(num[8]) % 2 == 0:
-            k1 = test(num[:9], range1)
-            if k1 == 10 or str(k1) != num[9]:
-                continue
-            k2 = test(num[:9]+str(k1), range2)
-            if k2 == 10 or str(k2) != num[10]:
-                continue
-            count += 1
-            print(num)
+    valid_date = filter(lambda num: int(num[:2]) <= 31 and num[2:4] == "08", numbers)
+    valid_female = filter(lambda num: int(num[8]) % 2 == 0, valid_date)
+    for num in valid_female:
+        k1 = test(num[:9], range1)
+        if k1 == 10 or str(k1) != num[9]:
+            continue
+        k2 = test(num[:9]+str(k1), range2)
+        if k2 == 10 or str(k2) != num[10]:
+            continue
+        count += 1
+        print(num)
     print(count)
 
-main()
+if __name__ == "__main__":
+    main()
