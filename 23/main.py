02d85a2e69643be3a68674dd1721ed05ccf460c6
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-12-23 13:30:11 +0100

    cleaned up ranges and ifs

--- 23/main.py
+++ 23/main.py
@@ -11,16 +11,12 @@ def main():
 
     count = 0
     for num in numbers:
-        if num[2:4] == "08" and int(num[8]) % 2 == 0:
-            k1 = test(num[0:9], range1)
-            if k1 == 10:
+        if int(num[:2]) <= 31 and num[2:4] == "08" and int(num[8]) % 2 == 0:
+            k1 = test(num[:9], range1)
+            if k1 == 10 or str(k1) != num[9]:
                 continue
-            k2 = test(num[0:9]+str(k1), range2)
-            if k2 == 10:
-                continue
-            if str(k1) != num[9] or str(k2) != num[10]:
-                continue
-            if int(num[0:2]) > 31:
+            k2 = test(num[:9]+str(k1), range2)
+            if k2 == 10 or str(k2) != num[10]:
                 continue
             count += 1
             print(num)
